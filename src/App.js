ae435648e72468d54ef68f476799f6db141dc2cf
vivek chhabra <vivekcb8642@gmail.com>
2023-07-02 07:11:17 +0530

    add App component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Login from "./pages/login/Login";
+
+function App() {
+    return (
+        <div className="App">
+            <Navbar />
+            {/* <Login /> */}
+        </div>
+    );
+}
+
+export default App;
